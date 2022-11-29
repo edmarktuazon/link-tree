45af11fea7ff5531df92861022d0a1cece13a8ac
edmarktuazon <edmarktuazon03@gmail.com>
2022-11-29 18:38:42 +0800

    update commit

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -25,7 +25,7 @@ const load = () => {
 window.addEventListener("load", load);
 </script>
 <template>
-  <section class="min-h-[100vh] relative">
+  <section class="min-h-[100vh] grid place-items-center">
     <video
       autoplay
       loop
@@ -38,147 +38,145 @@ window.addEventListener("load", load);
       />
     </video>
     <div class="grid place-items-center" id="content">
-      <div class="grid place-items-center">
-        <figure class="grid place-items-center mt-12">
-          <img
-            :src="EdmarkImage"
-            alt="Edmark Image"
-            class="border border-1 border-white rounded-full p-2"
-          />
-          <figcaption
-            class="
-              text-white
-              w-36
-              bg-[#00000085]
-              grid
-              place-items-center
-              mt-5
-              rounded-full
-              p-1
-              text-sm
-              border border-1 border-white
-            "
-          >
-            {{ name }}
-          </figcaption>
-        </figure>
-        <!-- links -->
-        <div class="mt-8 mb-8 flex flex-col">
-          <RouterLink
-            to="/github"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[90vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ github }}
-          </RouterLink>
-          <RouterLink
-            to="/portfolio"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[80vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ portfolio }}
-          </RouterLink>
-          <RouterLink
-            to="/linkedin"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[80vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ linkedin }}
-          </RouterLink>
-          <RouterLink
-            to="/facebook"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[80vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ facebook }}
-          </RouterLink>
-          <RouterLink
-            to="/instagram"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[80vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ instagram }}
-          </RouterLink>
-          <RouterLink
-            to="/email"
-            class="
-              text-black
-              decoration-0
-              bg-[#ffffffd0]
-              mt-5
-              p-4
-              text-center
-              rounded-full
-              capitalize
-              w-full
-              min-w-[80vw]
-              md:w-full md:min-w-[60vw]
-              lg:w-full lg:min-w-[30vw]
-            "
-          >
-            {{ email }}
-          </RouterLink>
-        </div>
+      <figure class="grid place-items-center">
+        <img
+          :src="EdmarkImage"
+          alt="Edmark Image"
+          class="border border-1 border-white rounded-full p-2"
+        />
+        <figcaption
+          class="
+            text-white
+            w-36
+            bg-[#00000085]
+            grid
+            place-items-center
+            mt-5
+            rounded-full
+            p-1
+            text-sm
+            border border-1 border-white
+          "
+        >
+          {{ name }}
+        </figcaption>
+      </figure>
+      <!-- links -->
+      <div class="mt-8 flex flex-col">
+        <RouterLink
+          to="/github"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[90vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ github }}
+        </RouterLink>
+        <RouterLink
+          to="/portfolio"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[80vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ portfolio }}
+        </RouterLink>
+        <RouterLink
+          to="/linkedin"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[80vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ linkedin }}
+        </RouterLink>
+        <RouterLink
+          to="/facebook"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[80vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ facebook }}
+        </RouterLink>
+        <RouterLink
+          to="/instagram"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[80vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ instagram }}
+        </RouterLink>
+        <RouterLink
+          to="/email"
+          class="
+            text-black
+            decoration-0
+            bg-[#ffffffd0]
+            mt-5
+            p-4
+            text-center
+            rounded-full
+            capitalize
+            w-full
+            min-w-[80vw]
+            md:w-full md:min-w-[60vw]
+            lg:w-full lg:min-w-[30vw]
+          "
+        >
+          {{ email }}
+        </RouterLink>
       </div>
     </div>
   </section>
